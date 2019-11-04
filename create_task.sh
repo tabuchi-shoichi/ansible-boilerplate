33d985e610e8428ae4657a9fee15ea0774df7668
tabuchi-shoichi <52723654+tabuchi-shoichi@users.noreply.github.com>
2019-11-04 16:17:46 +0900

    Update create_task.sh

--- create_task.sh
+++ create_task.sh
@@ -1,8 +1,6 @@
-#プロンプトをechoを使って表示
 echo "Ente new task name"
-#入力を受付、その入力を「str」に代入
 read task_name
-#roles以下にディレクトリ作成
+
 mkdir ./roles/$task_name
 mkdir ./roles/$task_name/handlers
 mkdir ./roles/$task_name/tasks
